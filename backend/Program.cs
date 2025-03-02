cc8f88ebc599a84534e559e85b9b1f0acd1f5594
BezerraC <cbezerraneto@gmail.com>
2025-03-02 10:52:07 -0300

    chore: register controller & service

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddSingleton<RoverService>();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.MapGet("/", () => "API Explorando Marte");
 
