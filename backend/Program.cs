543118aac459c94d04ea78fff3953db69abb1454
BezerraC <cbezerraneto@gmail.com>
2025-03-02 14:38:40 -0300

    feat: CORS setup for frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS setup
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IMovementStrategy, StandardMovementStrategy>();
 builder.Services.AddSingleton<RoverService>();
 
 var app = builder.Build();
+
+app.UseCors("AllowAllOrigins");
 
 app.MapControllers();
 
