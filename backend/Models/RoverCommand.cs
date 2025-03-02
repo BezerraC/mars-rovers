2609176ad178b7aaf0f8cd478fe0962dd963b5a5
BezerraC <cbezerraneto@gmail.com>
2025-03-02 13:06:43 -0300

    fix: made RoverCommand fields non-nullable

--- backend/Models/RoverCommand.cs
+++ backend/Models/RoverCommand.cs
@@ -3,4 +3,11 @@ public class RoverCommand
     public Rover Rover { get; set; }
     public Plateau Plateau { get; set; }
     public string Commands { get; set; }
+
+    public RoverCommand(Rover rover, Plateau plateau, string commands)
+    {
+        Rover = rover;
+        Plateau = plateau;
+        Commands = commands;
+    }
 }
